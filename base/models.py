a23a4be03717019e45de0a4d75a290513fa2e07e
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-10-22 12:13:37 +0300

    create the Reviews model

--- base/models.py
+++ base/models.py
@@ -19,3 +19,15 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(default=0, null=True, blank=True)
+    comment = models.TextField(null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.rating)
