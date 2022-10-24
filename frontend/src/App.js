991871643f754a9e6477edf7deaf1c3a851fe854
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-10-24 14:37:39 +0300

    Frontend: create cart reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ function App() {
             <Route path='/home' element={ <HomeScreen /> } />
             <Route path='/product/:id' element={ <ProductScreen /> } />
             <Route path='/cart/:id' element={ <CartScreen /> } />
+            <Route path='/cart' element={ <CartScreen /> } />
           </Routes>
         </Container>
       </main>
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,20 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useLocation, useParams } from 'react-router-dom'
 
 function CartScreen() {
+
+  const params = useParams()
+  const productId = params.id
+
+  const location = useLocation()
+  const searchParameters = new URLSearchParams(location.search)
+  const qty = searchParameters.get('qty') ? searchParameters.get('qty') : ''
+
+  console.log(productId, '...........', qty)
+
+  const dispatch = useDispatch()
+
   return (
     <div>
       <h1>Cart</h1>
