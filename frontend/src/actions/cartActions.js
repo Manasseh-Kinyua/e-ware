e53e34465aa312244286f27b04502a09fbdb68b8
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-10-24 16:01:27 +0300

    Frontend: create cart remove item action

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 import axios from "axios";
 import { PRODUCT_DETAILS_ENDPOINT } from "../constants/apiConstants";
 
@@ -14,6 +14,14 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
             countInStock: data.countInStock,
             qty
         }
+    })
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const removeFromCart = (id) => async (dispatch, getState) => {
+    dispatch({
+        type: CART_REMOVE_ITEM,
+        payload: id
     })
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
