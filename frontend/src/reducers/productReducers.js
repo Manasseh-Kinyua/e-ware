a1ed3ab2c0427b2a1ab844f94606b0e34e0eacfa
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-10-24 10:20:11 +0300

    Frontend: create the productDetails reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,6 +2,10 @@ import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAIL,
+
+    PRODUCT_DETAIL_REQUEST,
+    PRODUCT_DETAIL_SUCCESS,
+    PRODUCT_DETAIL_FAIL,
 } from "../constants/productConstants";
 
 export const productListReducer = (state = {products: []}, action) => {
@@ -19,6 +23,31 @@ export const productListReducer = (state = {products: []}, action) => {
             }
 
         case PRODUCT_LIST_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+        default:
+            return state
+    }
+}
+
+export const productDetailsReducer = (state = {product: {reviews: []}}, action) => {
+    switch(action.type) {
+        case PRODUCT_DETAIL_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
+
+        case PRODUCT_DETAIL_SUCCESS:
+            return {
+                loading: false,
+                products: action.payload
+            }
+
+        case PRODUCT_DETAIL_FAIL:
             return {
                 loading: false,
                 error: action.payload
