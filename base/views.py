b69fce0419f6c1e52a95ef2a14e95855f28dcc9d
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-10-25 10:36:11 +0300

    Backend: create the get all users endpoint

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
@@ -41,9 +43,17 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
