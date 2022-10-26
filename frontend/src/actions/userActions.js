659e57346ac5feef79ae211ab0e1d1607b997465
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-10-26 11:05:01 +0300

    Frontend: create userRegister action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,10 +2,56 @@ import {
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
     USER_LOGIN_FAIL,
+
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL,
+
     USER_LOGOUT,
 } from "../constants/userConstants";
 import axios from 'axios'
-import { USER_LOGIN_ENDPOINT } from "../constants/apiConstants";
+import { USER_LOGIN_ENDPOINT, USER_REGISTER_ENDPOINT } from "../constants/apiConstants";
+
+export const register = (name, email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_REGISTER_REQUEST
+        })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const {data} = await axios.post(
+            USER_REGISTER_ENDPOINT,
+            {'name': name, 'email': email, 'password': password},
+            config
+        )
+
+        dispatch({
+            type: USER_REGISTER_SUCCESS,
+            payload: data
+        })
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch(error) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message
+        })
+    }
+}
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem('userInfo')
+    dispatch({
+        type: USER_LOGOUT
+    })
+}
 
 export const login = (email, password) => async (dispatch) => {
     try {
@@ -39,11 +85,4 @@ export const login = (email, password) => async (dispatch) => {
                 : error.message
         })
     }
-}
-
-export const logout = () => (dispatch) => {
-    localStorage.removeItem('userInfo')
-    dispatch({
-        type: USER_LOGOUT
-    })
 }
