14798309d146d6db76b15b138c188bedebfa8f8e
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-10-27 11:24:57 +0300

    Frontend: create the order create action

---
+++ frontend/src/actions/orderActions.js
@@ -0,0 +1,46 @@
+import {
+    ORDER_CREATE_REQUEST,
+    ORDER_CREATE_SUCCESS,
+    ORDER_CREATE_FAIL,
+} from "../constants/orderConstants";
+import axios from 'axios'
+import { CREATE_ORDER_ENDPOINT } from "../constants/apiConstants";
+
+export const createOrder = (order) => async (dispatch, getState) => {
+    try {
+
+        dispatch({
+            type: ORDER_CREATE_REQUEST
+        })
+    
+        const {
+            userLogin: {userInfo}
+        } = getState()
+    
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+    
+        const {data} = await axios.post(
+            CREATE_ORDER_ENDPOINT,
+            order,
+            config
+        )
+    
+        dispatch({
+            type: ORDER_CREATE_SUCCESS,
+            payload: data
+        })
+
+    }catch(error) {
+        dispatch({
+            type: ORDER_CREATE_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message
+        })
+    }
+}
--- frontend/src/constants/apiConstants.js
+++ frontend/src/constants/apiConstants.js
@@ -7,3 +7,5 @@ export const USER_LOGIN_ENDPOINT = `${BASE_URL}users/login/`
 export const USER_REGISTER_ENDPOINT = `${BASE_URL}users/register/`
 export const GET_USER_PROFILE_ENDPOINT = `${BASE_URL}users/`
 export const UPDATE_USER_PROFILE_ENDPOINT = `${BASE_URL}users/profile/update/`
+
+export const CREATE_ORDER_ENDPOINT = `${BASE_URL}orders/add/`
