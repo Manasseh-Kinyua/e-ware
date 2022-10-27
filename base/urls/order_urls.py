fb2a29511d53a3956b9830d51f1d72aeec2a6874
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-10-27 15:56:06 +0300

    Backend: create update to paid endpoint

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -3,5 +3,6 @@ from base.views import order_views as views
 
 urlpatterns = [
     path('add/', views.addOrderItems, name='order-add'),
-    path('<str:pk>/', views.getOrderById, name='order-detail')
+    path('<str:pk>/', views.getOrderById, name='order-detail'),
+    path('<str:pk>/pay/', views.updateToPaid, name='order-pay')
 ]
--- base/views/order_views.py
+++ base/views/order_views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from base.models import Product, Order, OrderItem, ShippingAddress
 from base.serializers import ProductSerializer, OrderSerializer
 from rest_framework import status
+from datetime import datetime
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -67,3 +68,14 @@ def getOrderById(request, pk):
             return Response({'detail': 'You are not authorized to see this order'}, status=status.HTTP_400_BAD_REQUEST)
     except:
         return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view({'PUT'})
+@permission_classes([IsAuthenticated])
+def updateToPaid(request, pk):
+    order = Order.objects.get(_id=pk)
+
+    order.isPaid = True
+    order.paidAt = datetime.now()
+
+    order.save()
+    return Response('Order has been paid')
