b3be97265a22a9d4280a389beb62b9235dce8420
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-10-27 14:44:06 +0300

    Frontend: create get order by id reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productDetailsReducer, productListReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userDetailsReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer } from './reducers/userReducers'
-import { orderCreateReducer } from './reducers/orderReducers'
+import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -18,6 +18,7 @@ const reducer = combineReducers({
     userUpdateProfile: userUpdateProfileReducer,
 
     orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
