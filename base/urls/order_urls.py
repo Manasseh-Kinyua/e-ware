c5dc6a0792731f8a871db132d034acd4a2f93f1b
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-10-27 16:00:26 +0300

    Frontend: create order pay reducer

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -4,5 +4,5 @@ from base.views import order_views as views
 urlpatterns = [
     path('add/', views.addOrderItems, name='order-add'),
     path('<str:pk>/', views.getOrderById, name='order-detail'),
-    path('<str:pk>/pay/', views.updateToPaid, name='order-pay')
+    path('<str:pk>/pay/', views.updateToPaid, name='order-pay'),
 ]
