195068ed5ff37e4b0a6c1905a4c618e26f0b8a79
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-10-27 10:41:41 +0300

    Backend: create order view and url

---
+++ base/views/order_views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from base.models import Product, Order, OrderItem, ShippingAddress
+from base.serializers import ProductSerializer, OrderSerializer
+from rest_framework import status
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+
+    orderItems = data['orderItems']
+
+    if orderItems and len(orderItems) == 0:
+        return Response({'detail': 'No order Items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        # (1) create order
+        order = Order.objects.create(
+            user=user,
+            paymentMethod=data['paymentMethod'],
+            taxPrice=data['taxPrice'],
+            shippingPrice=data['shippingPrice'],
+            totalPrice=data['totalPrice']
+        )
+        # (1) create shipping address
+        shippingAddress = ShippingAddress.objects.create(
+            order=order,
+            address=data['shippingAddress']['address'],
+            city=data['shippingAddress']['city'],
+            postalCode=data['shippingAddress']['postalCode'],
+            country=data['shippingAddress']['country']
+        )
+        # (1) create order items and set order to orderItem relationship
+        for i in orderItems:
+            product = Product.objects.get(_id=1['product'])
+
+            item = OrderItem.objects.create(
+                product=product,
+                order=order,
+                name=product.name,
+                qty=i['qty'],
+                price=i['price'],
+                image=product.image.url
+            )
+            # (1) update stock
+            product.countInStock -= int(item.qty)
+            product.save()
+
+    serializer = OrderSerializer(order, many=False)
+    return Response(serializer.data)
