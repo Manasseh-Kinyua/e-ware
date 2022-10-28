7a684c322089debee63ae537c18ef2ca71316a97
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-10-28 11:34:40 +0300

    Frontend: dispatch list users action

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -131,7 +131,7 @@ export const userListReducer = (state = {users:[]}, action) => {
         case USER_LIST_SUCCESS:
             return {
                 loading: false,
-                userS: action.payload
+                users: action.payload
             }
 
         case USER_LIST_FAIL:
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Table, Button, Row } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { listUsers } from '../actions/userActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 function UserListScreen() {
+
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
+
+    const userList = useSelector(state => state.userList)
+    const {loading, error, users} = userList
+
+    const navigate = useNavigate()
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if(userInfo && userInfo.isAdmin) {
+            dispatch(listUsers())
+        } else {
+            navigate('/profile')
+        }
+    }, [dispatch, userInfo])
+
+    const deleteHandler = (id) => {
+        console.log('delete', id)
+    }
+
   return (
     <div>
       <h1>Users</h1>
+      {loading ? (
+        <Loader/>
+      ) : error ? (
+        <Message severity='error' error={error} />
+      ) : (
+        <Table responsive hover striped className='body-bg table-sm'>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>NAME</th>
+                    <th>EMAIL</th>
+                    <th>ADMIN</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map(user => (
+                    <tr key={user._id}>
+                    <td>{user._id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    {user.isAdmin ? (
+                        <td><i className="fa-solid fa-check text-success"></i></td>
+                    ) : (
+                        <td><i className="fa-solid fa-xmark text-danger"></i></td>
+                    )}
+                    <td>
+                        <Link to={`/admin/user/${user._id}/edit`}>
+                            <Button className='btn-sm bg'><i className="fa-sharp fa-solid fa-pen-to-square"></i></Button>
+                        </Link>
+                    </td>
+                    <td>
+                        <Button
+                            className='bg btn-sm'
+                            onClick={() => deleteHandler(user._id)}>
+                            <i className="fa-solid fa-trash"></i>
+                        </Button>
+                    </td>
+                </tr>
+                ))}
+            </tbody>
+        </Table>
+      )}
     </div>
   )
 }
