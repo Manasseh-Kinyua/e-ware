922aa8b6360cb93f2a6144cf005ca7eb2647bd48
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-10-31 09:43:54 +0300

    Backend: create the get orders view and url

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from base.views import order_views as views
 
 urlpatterns = [
+    path('', views.getOrders, name='orders'),
     path('add/', views.addOrderItems, name='order-add'),
     path('myorders/', views.getMyOrders, name='my-order'),
     path('<str:pk>/', views.getOrderById, name='order-detail'),
--- base/views/order_views.py
+++ base/views/order_views.py
@@ -64,6 +64,13 @@ def getMyOrders(request):
     return Response(serializer.data)
 
 @api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getOrders(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getOrderById(request, pk):
     user = request.user
